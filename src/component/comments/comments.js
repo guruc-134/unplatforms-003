7b01bae5b80b6cb38b371fc1db94f4003571b634
Guru Charan <guru.134charan@gmail.com>
2022-07-23 23:17:42 +0530

    minor style changes

--- src/component/comments/comments.js
+++ src/component/comments/comments.js
@@ -71,7 +71,8 @@ function Comments({comments, commentOnPost}) {
           <hr></hr>
           <Paper
             component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}>
+            className='textField'
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 380}}>
               <InputBase
                 sx={{ ml: 1, flex: 1 }}
                 value={commentContent}
